8feac8fe2fc17be12e41b1be71fef9d09633b3ff
Dan <109541525+danofit@users.noreply.github.com>
2024-11-25 14:59:14 -0500

    test

--- thehtml/thejavascript/slideout-menu.js
+++ thehtml/thejavascript/slideout-menu.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var slideoutMenu = document.querySelector('.slideout-menu');
-  
-    menuToggle.addEventListener('click', function() {
-      if (slideoutMenu.style.left === '-250px') {
-        slideoutMenu.style.left = '0';
+  var menuToggle = document.querySelector('.menu-toggle');
+  var slideoutMenu = document.querySelector('.slideout-menu');
+
+  menuToggle.addEventListener('click', function() {
+      if (slideoutMenu.style.left === '-250px' || !slideoutMenu.style.left) {
+          slideoutMenu.style.left = '0';
       } else {
-        slideoutMenu.style.left = '-250px';
+          slideoutMenu.style.left = '-250px';
       }
-    });
   });
-  
+});
